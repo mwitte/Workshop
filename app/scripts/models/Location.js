b03e7de3cde8c90a2d4b4b979ad100b514a1a7c6
Matthias Witte <m.witte@techdivision.com>
2014-04-09 16:10:07 +0200

    map updates weather data

--- app/scripts/models/Location.js
+++ app/scripts/models/Location.js
@@ -12,17 +12,34 @@ Weather.Location = Ember.Object.extend({
     this.updateWeather();
   },
 
+  theCoordinatesChanged: function() {
+    this.updateWeather();
+  }.observes('coordinates'),
+
   updateWeather: function() {
     var _this = this;
+
+    var params = {
+      units: 'metric'
+    };
+
+    if(_this.get('coordinates.lat')) {
+      params.lat = this.get('coordinates.lat')
+      params.lon = this.get('coordinates.lon')
+    }else{
+      params.q = this.get('name');
+    }
+
     Ember.$.ajax({
       url: 'http://api.openweathermap.org/data/2.5/weather',
-      data: {
-        q: this.get('name'),
-        units: 'metric'
-      }
+      data: params
     }).done(function(response){
       _this.set('weather', Ember.Object.create(response.main));
-      _this.set('coordinates', Ember.Object.create(response.coord));
+      _this.set('name', response.name);
+
+      if(!_this.get('coordinates.lat')) {
+        _this.set('coordinates', Ember.Object.create(response.coord));
+      }
     });
   }
 
--- app/scripts/views/MapView.js
+++ app/scripts/views/MapView.js
@@ -5,14 +5,23 @@ Weather.MapView = Ember.View.extend({
   templateName: 'map',
 
   didInsertElement: function() {
-
+    var _this = this;
     var domOfThisView = this.get('element');
-    console.log(domOfThisView);
 
     var mapOptions = {
       zoom: 8,
-      center: new google.maps.LatLng(-34.397, 150.644)
+      center: new google.maps.LatLng(this.get('position.lat'), this.get('position.lon'))
     };
     map = new google.maps.Map(domOfThisView, mapOptions);
+
+    google.maps.event.addListener(map, 'idle', function() {
+      var currentPosition = map.getCenter();
+
+      _this.set('position', Ember.Object.create({
+        lat: currentPosition.k,
+        lon: currentPosition.A
+      }));
+    });
   }
+
 });
